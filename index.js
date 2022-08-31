bde5feaf11c1a1b08e2b097ea07d3b993734b99d
Jelena Djurdjevic <+>
2022-08-31 10:34:45 -0600

    commit tree 6a0c2778e8b94879f900b69ff02cc52cb1882d58

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+const random = Math.ceil(Math.random()*10);
+let num3 = 46;
+let num4 = 6;
+let mod = num3 % num4; 
+const max = Math.max(4,8,12,20);
